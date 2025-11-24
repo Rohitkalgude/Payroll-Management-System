7ddea2a9b695def014d67d5cc293417ec79e6a3a
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-11-24 13:59:34 +0530

    Refactor server.js for improved code readability and consistency

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,32 @@
 const express = require("express");
-const dotenv = require('dotenv');   
-const connectDB = require('./config/dbconnection');
-const adminRoutes = require('./routes/adminRoutes');
-const employeeRoutes = require('./routes/employeeRoutes');
-const cors = require('cors');
+const dotenv = require("dotenv");
+const connectDB = require("./config/dbconnection");
+const adminRoutes = require("./routes/adminRoutes");
+const employeeRoutes = require("./routes/employeeRoutes");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
-app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send("the api is runnuing")
-})
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
-app.use('/api/admin',adminRoutes)
-app.use('/api/employee',employeeRoutes)
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("the api is runnuing");
+});
 
-const port =process.env.PORT
-app.listen({port}, ()=>{
-    console.log(`server started on ${port}`);
-})
+app.use("/api/admin", adminRoutes);
+app.use("/api/employee", employeeRoutes);
+
+const port = process.env.PORT;
+app.listen({ port }, () => {
+  console.log(`server started on ${port}`);
+});

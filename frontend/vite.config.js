af73bda1f5bbccc4c846b59e8e0a97fd7227396b
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-12-03 22:12:30 +0530

    vite config remove

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // default dev port
-    open: true, // opens browser automatically
-    proxy: {
-      // only if you want to call localhost backend without changing URLs
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  define: {
-    "process.env": process.env, // makes process.env available in frontend
-  },
-});
+})
